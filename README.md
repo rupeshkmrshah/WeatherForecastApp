3fea75892b8392c8fd9b6470205b114a4d0f2fca
Rupesh Shah <124813194+rupeshkmrshah@users.noreply.github.com>
2024-05-17 18:12:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weatherforecast
+ This React-based app allows you to search for any city and instantly retrieve weather information, including temperature, humidity, wind speed, and more.☀️🌧️❄️
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './components/img/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Weather from './components/WeatherForecast'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Weather/>
+    </div>
+  );
+}
+
+export default App
